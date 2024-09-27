4ead040de6302b5de9c828d336b4552ccc5fd045
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-09-28 03:55:45 +0530

    Implemented Interfaces

--- main.go
+++ main.go
@@ -7,21 +7,83 @@ import (
 	"strings"
 
 	"example.com/note/entity"
+	"example.com/note/todo"
 )
 
+type saver interface {
+	Save() error
+}
+
+type outputtable interface {
+	saver
+	Display()
+}
+
 func main() {
+	result := add(5, 3)
+	fmt.Println("Result is : ", result)
+	resultFloat := add(5.4, 3.7)
+	fmt.Println("Result is : ", resultFloat)
+	resultStr := add("Hello ", "JI")
+	fmt.Println("Result is : ", resultStr)
+
 	title, content := getNoteData()
+	todoText := getUserInput("Todo Text: ")
+	todo, err := todo.New(todoText)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = SaveAndOutputData(todo)
+	if err != nil {
+		return
+	}
 	userNote, err := entity.New(title, content)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	userNote.Display()
-	err = userNote.Save()
+	err = SaveAndOutputData(userNote)
+	if err != nil {
+		return
+	}
+}
+
+func add[T string | int | float32 | float64](a, b T) T {
+	return a + b
+}
+
+func printSomething(value interface{}) {
+	intVal, ok := value.(int)
+	if ok {
+		fmt.Println("Integer: ", intVal)
+		return
+	}
+	floatVal, ok := value.(float32)
+	if ok {
+		fmt.Println("Float : ", floatVal)
+		return
+	}
+	stringVal, ok := value.(string)
+	if ok {
+		fmt.Println("String: ", stringVal)
+		return
+	}
+}
+
+func SaveAndOutputData(data outputtable) error {
+	data.Display()
+	return data.Save()
+}
+
+func SaveData(data saver) error {
+	err := data.Save()
 	if err != nil {
 		fmt.Println("Saving the note failed")
+		return err
 	}
 	fmt.Println("Saving the Note Succeded")
+	return nil
 }
 
 func getNoteData() (string, string) {
